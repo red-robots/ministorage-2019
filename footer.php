857d404e88da95aea32106b2a81ae02009a9ea9d
lisadebona <annalisa.quiamco@gmail.com>
2019-04-15 16:59:16 -0600

    Updated footer

--- footer.php
+++ footer.php
@@ -1,10 +1,14 @@
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
+	<?php  
+	$email_address = get_field('email_address','option');
+	?>
+	<footer id="colophon" class="site-footer clear" role="contentinfo">
 		<div class="wrapper">
-			<div class="site-info">
-				
-			</div>
+			<?php if ($email_address) { ?>
+				<span class="foot-info"><a href="mailto:<?php echo antispambot($email_address,1); ?>"><?php echo antispambot($email_address); ?></a></span>
+			<?php } ?>
+			<?php wp_nav_menu( array( 'menu' => 'Footer Menu', 'menu_id' => 'footer--menu', 'menu_class'=>'footer-menu menu', 'container'=>false ) ); ?>
 		</div>
 	</footer>
 </div><!-- #page -->
--- header.php
+++ header.php
@@ -18,6 +18,9 @@
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <script defer src="<?php bloginfo( 'template_url' ); ?>/assets/svg-with-js/js/fontawesome-all.js"></script>
 <?php wp_head(); ?>
+<?php $analytics = get_field('google_analytics','option'); ?>
+<?php if ($analytics) { ?><?php echo $analytics ?><?php } ?>
+
 </head>
 
 <body <?php body_class(); ?>>
@@ -36,7 +39,10 @@
 		        <?php } ?>
 				<nav id="site-navigation" class="main-navigation" role="navigation">
 					<span id="toggleMenu" class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'MENU', 'acstarter' ); ?><span><i></i></span></span>
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu', 'container_class'=>'main-menu-wrapper','link_before'=>'<span>','link_after'=>'</span>' ) ); ?>
+					<?php 
+						wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu', 'container_class'=>'main-menu-wrapper desktop','link_before'=>'<span>','link_after'=>'</span>' ) ); 
+						wp_nav_menu( array( 'menu' => 'Mobile Menu', 'menu_id' => 'mobile-primary-menu', 'container_class'=>'main-menu-wrapper mobile','link_before'=>'<span>','link_after'=>'</span>' ) ); 
+					?>
 					<?php 
 						$hdr_button_name = get_field('header_button_name','option'); 
 						$hdr_button_link = get_field('header_button_link','option'); 
