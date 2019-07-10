e6650d6c639787a91c9193bdbfdc34ac5ce2994e
Lisa <annalisa.quiamco@gmail.com>
2019-07-09 21:28:16 -0600

    Fixed recent news (3 items with the same content)

--- template-parts/content-news-list.php
+++ template-parts/content-news-list.php
@@ -14,6 +14,8 @@ if($recentPosts) {
     }
 }
 
+
+
 $perpage = 12;
 $paged = ( get_query_var( 'pg' ) ) ? absint( get_query_var( 'pg' ) ) : 1;
 $args = array(
@@ -25,33 +27,37 @@ $args = array(
     'post__not_in'     => $exclude_recent,
     'paged'            => $paged
     );
-$news = new WP_Query($args);
-if ( $news->have_posts() ) { ?>
+?>
 
-    <?php if ($paged<2) { ?>
-        <div class="recent-news-posts">
-            <div class="wrapper">
-                <div class="flexrow clear">
-                <?php foreach ($recentPosts as $r) { 
-                    $pid = $r->ID; ?>
-                    <div class="flexcol recent-post">
-                        <div class="inside">
-                            <h3 class="post-title"><a href="<?php echo get_permalink($pid)?>"><?php echo get_the_title($pid); ?></a></h3>
-                            <div class="post-excerpt">
-                                <?php if ($content = get_the_content($pid)) {
-                                    $s_content = strip_shortcodes($content);
-                                    $s_content = strip_tags($s_content); ?>
-                                    <?php echo shortenText($s_content,300); ?>
-                                <?php } ?>
-                            </div>
-                            <div class="post-link"><a href="<?php echo get_permalink($pid)?>">Continue Reading <span>&rarr;</span></a></div>
+<?php if ($paged==1) { ?>
+    <div class="recent-news-posts">
+        <div class="wrapper">
+            <div class="flexrow clear">
+            <?php foreach ($recentPosts as $r) { 
+                $pid = $r->ID; 
+                $content = $r->post_content; ?>
+                <div class="flexcol recent-post">
+                    <div class="inside">
+                        <h3 class="post-title"><a href="<?php echo get_permalink($pid)?>"><?php echo get_the_title($pid); ?></a></h3>
+                        <div class="post-excerpt">
+                            <?php if ($content) {
+                                $s_content = strip_shortcodes($content);
+                                $s_content = strip_tags($s_content); ?>
+                                <?php echo shortenText($s_content,300); ?>
+                            <?php } ?>
                         </div>
-                    </div>  
-                <?php } ?>
-                </div>
+                        <div class="post-link"><a href="<?php echo get_permalink($pid)?>">Continue Reading <span>&rarr;</span></a></div>
+                    </div>
+                </div>  
+            <?php } ?>
             </div>
         </div>
-    <?php } ?>
+    </div>
+<?php } ?>
+
+
+ <?php $news = new WP_Query($args);
+if ( $news->have_posts() ) { ?>
 
     <div class="flex-container wrapper news-listing">
         <div class="flexrow clear">
@@ -74,25 +80,24 @@ if ( $news->have_posts() ) { ?>
     </div>
 
     <?php
-        $total_pages = $news->max_num_pages;
-        if ($total_pages > 1){ ?>
+    $total_pages = $news->max_num_pages;
+    if ($total_pages > 1){ ?>
 
-            <div id="pagination" class="pagination wrapper">
-                <?php
-                    $pagination = array(
-                        'base' => @add_query_arg('pg','%#%'),
-                        'format' => '?paged=%#%',
-                        'mid-size' => 1,
-                        'current' => $paged,
-                        'total' => $total_pages,
-                        'prev_next' => True,
-                        'prev_text' => __( '<span class="fas fa-chevron-left"></span>' ),
-                        'next_text' => __( '<span class="fas fa-chevron-right"></span>' )
-                    );
-                    echo paginate_links($pagination);
-                ?>
-            </div>
+        <div id="pagination" class="pagination wrapper">
             <?php
-        }
-    ?>
+                $pagination = array(
+                    'base' => @add_query_arg('pg','%#%'),
+                    'format' => '?paged=%#%',
+                    'mid-size' => 1,
+                    'current' => $paged,
+                    'total' => $total_pages,
+                    'prev_next' => True,
+                    'prev_text' => __( '<span class="fas fa-chevron-left"></span>' ),
+                    'next_text' => __( '<span class="fas fa-chevron-right"></span>' )
+                );
+                echo paginate_links($pagination);
+            ?>
+        </div>
+        <?php
+    } ?>
 <?php } ?>
